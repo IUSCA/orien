7833ea9e9b6b0aaf8c71b22a033b888fb7d0164b
Ryan Long <ryanlong@iu.edu>
2025-01-15 08:09:36 -0600

    Add logging to batch download task for improved output tracking

--- workers/workers/tasks/bc2_batch_download.py
+++ workers/workers/tasks/bc2_batch_download.py
@@ -1,6 +1,8 @@
 import workers.cmd as cmd
 from workers.config import config
+from celery.utils.log import get_task_logger
 
+logger = get_task_logger(__name__)
 
 def batch_download(celery_task, batch_id, **kwargs):
 
@@ -9,6 +11,9 @@ def batch_download(celery_task, batch_id, **kwargs):
   stdout, stderr = cmd.execute(['python', f'{batch_script}', f'{batch_id}'])
 
   print(f'STDOUT: {stdout} STDERR: {stderr}')
+
+
+  logger.info(f'STDOUT: {stdout} STDERR: {stderr}')
 
   # if stderr:
   #   raise Exception(stderr)
